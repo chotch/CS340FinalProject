0b387a8e4970e10c3c1bd7ee3a6e1eac2710ec8a
Connor <whitbey35@gmail.com>
2020-05-28 20:42:13 -0700

    removed unecessary code

--- Home.php
+++ Home.php
@@ -9,45 +9,14 @@
 
     <h1 class="logo">Travel Tracker</h1>
 
-<div class="container" >
-<!-- action to new page? after submission -->
-<!-- must adjust the href location so that it changes to the new html page -->
-  <button onclick="window.location.href= 'ViewTrips.php'"> View Trips </button>
-  <button onclick="window.location.href = 'AddTrip.php'"> Add Trip </button>
-  <button onclick="window.location.href = 'QuickStats.php'"> Quick Stats </button>
+    <div class="container" >
+    <!-- action to new page? after submission -->
+    <!-- must adjust the href location so that it changes to the new html page -->
+      <button onclick="window.location.href= 'ViewTrips.php'"> View Trips </button>
+      <button onclick="window.location.href = 'AddTrip.php'"> Add Trip </button>
+      <button onclick="window.location.href = 'QuickStats.php'"> Quick Stats </button>
 
-</div>
-
-
-
-
-<script>
-function myFunction() {
-    var Uname = document.getElementById("frm1")[0].value;
-    var Pass = document.getElementById("frm1")[1].value;
-    var CPass = document.getElementById("frm1")[2].value;
-    var Phone = document.getElementById("frm1")[3].value;
-
-    var checkBox = document.getElementById("tos");
-
-    if(Pass != CPass){
-        alert("You entered two different passwords.");
-    }else if(Pass.length < 7){
-        alert("Your password is not long enough.");
-    }else if(Uname.length < 6){
-        alert("Your username is not long enough.");
-    }else if(!checkBox.checked){
-        alert("You need to accept our Terms of Service.");
-    }else{
-        alert("Creating account with \nUsername: " + Uname + "\nPassword: " + Pass);
-    }
-
-
-	    //var element = document.getElementById("p1");
-    //element.innerHTML = fname;
-
-}
-</script>
+    </div>
 
 </body>
 </html>
