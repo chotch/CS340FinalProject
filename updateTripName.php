d3a479ea8d3f7d4b1863f5e2a59616e5263115e3
chotch <43656383+chotch@users.noreply.github.com>
2020-06-02 19:19:35 -0700

    All update correctly.

--- updateTripName.php
+++ updateTripName.php
@@ -10,12 +10,31 @@ $name = $_REQUEST['tripname'];
 $rating = $_REQUEST['rating'];
 $cost = $_REQUEST['cost'];
 
+if ($name != ''){
+  $sql = "UPDATE Trip
+          SET trip_name = '".$name."'
+          WHERE trip_id = '".$id."'";
+  mysqli_query($link, $sql);
+}
 
+if ($rating != ''){
+  $sql = "UPDATE Trip
+          SET  rating = $rating
+          WHERE trip_id = '".$id."'";
+  mysqli_query($link, $sql);
+}
 
-$sql = "UPDATE Trip
-        SET cost = $cost, rating = $rating, trip_name = '".$name."'
-        WHERE trip_id = '".$id."'";
-mysqli_query($link, $sql);
+if ($cost != ''){
+  $sql = "UPDATE Trip
+          SET cost = $cost
+          WHERE trip_id = '".$id."'";
+  mysqli_query($link, $sql);
+}
+
+//$sql = "UPDATE Trip
+  //      SET cost = $cost, rating = $rating, trip_name = '".$name."'
+    //    WHERE trip_id = '".$id."'";
+//mysqli_query($link, $sql);
 
  echo "<script>location.href='ViewTrips.php'</script>";
 
