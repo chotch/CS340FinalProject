b9eb54e8a24ecf51b5f40fa3eca9c2523fbc09d7
chotch <43656383+chotch@users.noreply.github.com>
2020-06-08 10:23:30 -0700

    Doesn't work yet:( want to send select option valu

--- Home.php
+++ Home.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
+      <script src="js/jquery-3.1.1.min.js"></script>
+      <script>
+      $(document).ready(function (){
+        $("#usr").change(function (){
+          var selectedOption = $("#usr option:selected").val();
+          alert("Hi, " + selectedOption);
+        });
+      });
+      </script>
         <style><?php include 'Home.css'; ?></style>
         <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@500&display=swap" rel="stylesheet">
         <title>Travel Tracker</title>
@@ -10,13 +19,48 @@
     <h1 class="logo">Travel Tracker</h1>
 
     <div class="container" >
-    <!-- action to new page? after submission -->
-    <!-- must adjust the href location so that it changes to the new html page -->
+      <label for="user"> Select a Traveller </label>
+
+      <select name="usr" id="usr">
+        <?php
+         require_once 'config.php';
+         $sql = "SELECT traveller_name, traveller_ID FROM Traveller";
+         $result = mysqli_query($link, $sql);
+         while ($row = mysqli_fetch_array($result)){
+           echo '<option value="'.$row['traveller_ID'].'"> ' . $row['traveller_name'];
+           echo '</option>';
+         }
+
+         echo '</select>';
+
+      //   $newUSRID=$_GET['usr'];
+      //   print($newUSRID);
+         echo '</div>';
+         $myID = $_POST['usr'];
+        echo '<div class="container">';
+        print("my id is:");
+        print($myID);
+       //echo " <button onclick="window.location.href= 'ViewTrips.php'"> View Trips </button>";
+      //  echo ' <button onclick="window.location.href = 'AddTrip.php'"> Add Trip </button>';
+      //  echo ' <button onclick="window.location.href = 'QuickStats.php'"> Quick Stats </button>';
+        echo "<a href='ViewTrips.php?newUSRID=" . $myID . "' ><button> View Trips</button></a>";
+        echo "<a href='AddTrip.php?usrid=" . $newUSRID . "' ><button>Add Trip</button></a>";
+        echo "<a href='QuickStats.php?usrid=" . $newUSRID . "' ><button>Quick Stats</button></a>";
+
+        echo '</div>';
+         ?>
+    <!--   </select>
+     </div>
+     <div class="container">
+     action to new page? after submission -->
+    <!-- must adjust the href location so that it changes to the new html page
+
       <button onclick="window.location.href= 'ViewTrips.php'"> View Trips </button>
       <button onclick="window.location.href = 'AddTrip.php'"> Add Trip </button>
       <button onclick="window.location.href = 'QuickStats.php'"> Quick Stats </button>
 
-    </div>
+    </div> -->
+
 
 </body>
 </html>
